9c517e27c89004f8d125465e6d199cb8f2455b12
gowthamiallu5 <gowthami.allu@augmentolabs.com>
2020-10-30 18:47:21 +0530

    deleted class

--- PEImportMapping.API/Controllers/ImportMappingController.cs
+++ PEImportMapping.API/Controllers/ImportMappingController.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
-
+using PEImportMapping.API.Models;
 
 namespace PEImportMapping.API.Controllers
 {
@@ -13,12 +13,14 @@ namespace PEImportMapping.API.Controllers
     [ApiController]
     public class ImportMappingController : ControllerBase
     {
+        public ForgeModel ForgeModelobj;
+
        [HttpPost]
         public Object ImportFile()
         {
             var myJsonString = System.IO.File.ReadAllText("ZouraPayloads/CreateSubscription.json");
             var myJObject = JObject.Parse(myJsonString);
-
+            ForgeModelobj = new ForgeModel();
             return myJObject;
 
         }
