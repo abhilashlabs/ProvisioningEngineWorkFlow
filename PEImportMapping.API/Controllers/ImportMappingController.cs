5095aec161b4d933ffd43a6c8b05eb66bbea0c0b
gowthamiallu5 <gowthami.allu@augmentolabs.com>
2020-10-30 17:00:30 +0530

    Prepare workflow configuration (hardcoded values for Tenant recipe and Entitlements catalog)

---
+++ PEImportMapping.API/Controllers/ImportMappingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace PEImportMapping.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImportMappingController : ControllerBase
+    {
+       [HttpPost]
+        public Object ImportFile()
+        {
+            var myJsonString = System.IO.File.ReadAllText("ZouraPayloads/CreateSubscription.json");
+            var myJObject = JObject.Parse(myJsonString);
+
+            return myJObject;
+
+        }
+
+    }
+}
---
+++ PEImportMapping.API/Models/ForgeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PEImportMapping.API.Models
+{
+    public class ForgeModel
+    {
+
+        public string HeaderEventType { get; set; }
+        public string UltimateParentAccountName  { get; set; }
+        public string LegalName  { get; set; }
+        public string AccountName  { get; set; }
+
+        public string EntentitlementCatelog { get; set; }
+         public string TenantRecipe { get; set; }
+    }
+}
