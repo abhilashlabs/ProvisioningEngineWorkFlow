e45b4921419e2b041e91137aa4f9a8846a57168a
anishs293 <anishs293@outlook.com>
2020-10-30 18:42:41 +0530

    alpha

--- PEImportMapping.API/PEImportMapping.Buisness/ImportMapping.cs
+++ PEImportMapping.API/PEImportMapping.Buisness/ImportMapping.cs
@@ -5,13 +5,13 @@ using System.Threading.Tasks;
 
 namespace PEImportMapping.API.PEImportMapping.Buisness
 {
-    public class ImportMapping:IImportMapping
+    public class ImportMapping: IImportMapping
     {
         public Object ImportFile()
         {
-            var myJsonString = System.IO.File.ReadAllText("ZouraPayloads/CreateSubscription.json");
-            var myJObject = JObject.Parse(myJsonString);
-            ForgeModelobj = new ForgeModel();
+
+            var myJObject = "";
+           
             return myJObject;
 
         }
